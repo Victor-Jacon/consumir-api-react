f21dc2ec9797250c6cf58bee0ec8ec8941e75b04
Victor Jacon <74216766+Victor-Jacon@users.noreply.github.com>
2021-09-29 20:52:47 -0300

    react-router: conhecendo hashRouter

--- src/react-router-dom/PageLearnReactRouterDom.jsx
+++ src/react-router-dom/PageLearnReactRouterDom.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
-import { BrowserRouter, Link, Switch, Route, useHistory, useLocation, NavLink, Redirect, Prompt, withRouter } from 'react-router-dom'
+import { HashRouter, BrowserRouter, Link, Switch, Route, useHistory, useLocation, NavLink, Redirect, Prompt, withRouter } from 'react-router-dom'
 import queryString from 'query-string'
 
 const PageLearnReactRouterDom = () => {
